407bf55eabf3b25353c76a25ed57333c407d8f10
mt-hata <s2422120@stu.musashino-u.ac.jp>
2024-12-03 23:55:13 +0900

    初回コミット

---
+++ sql/2422120_1.sql
@@ -0,0 +1,3 @@
+select *
+from minato_restaurant
+where block =  '南麻布四丁目';
---
+++ sql/2422120_3.sql
@@ -0,0 +1,13 @@
+SELECT 
+    DATE_PART('month', init_license_dt) AS month,
+    business_type,
+    COUNT(*) AS store_count
+FROM 
+    minato_restaurant
+WHERE 
+    DATE_PART('year', init_license_dt) = 2022
+GROUP BY 
+    DATE_PART('month', init_license_dt),
+    business_type
+ORDER BY 
+    month ASC, store_count DESC;
